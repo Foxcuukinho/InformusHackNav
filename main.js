8723e4cf0863bf551795a07e3937179e721df283
fox <uh?>
2026-04-01 22:58:50 -0300

    vault backup: 2026-04-01 22:58:50

--- main.js
+++
@@ -1,4 +0,0 @@
-const url = "https://www.gov.br/secom/pt-br/acompanhe-a-secom/noticias/2026/03/governo-do-brasil-publicara-orientacoes-para-garantir-implementacao-do-eca-digital";
-const html = await fetch(url).then(r => r.text());
-const text = html.replace(/<[^>]*>/g, " ").replace(/\s+/g, " ").trim();
-console.log(text.slice(0, 2000));
